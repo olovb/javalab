8e206737763ff775ff8b7c11a067463bbc9eb706
John Olov Brötell <jolovb@gmail.com>
2015-05-25 16:18:17 -0300

    Improved sample comments.

--- src/test/java/DefaultAbstractTest.java
+++ src/test/java/DefaultAbstractTest.java
@@ -1,4 +1,13 @@
 
+// Inheriting from interfaces: the 'default-default' and 
+// 'default-abstract' rules means an inheriting type sometimes
+// has to reimplement or redeclare an inherited method.
+
+/*----------------------------=o=------------------------------*/
+
+// Interfaces all having some declaration of an override-
+// equivalent method.
+
 interface IHello1 {
     default String hello() { return "Hello from IHello1."; } 
 }
@@ -13,9 +22,18 @@ interface IHello3 {
 
 /*----------------------------=o=------------------------------*/
 
+// Another override-equivalent declaration of the metod found
+// in the above interfaces.
+
+abstract class Hello1 {
+    abstract String hello();   
+}
+
+/*----------------------------=o=------------------------------*/
+
 // Interfaces inheriting multiple unrelated default methods or 
 // a combination of abstract and default methods that are
-// override equivalent must reimplement or redefine the methods
+// override equivalent must reimplement or redeclare the methods
 // in question. The hello() method below may not be omitted.
 
 interface IHello11 extends IHello1, IHello2 {
@@ -36,15 +54,9 @@ interface IHello22 extends IHello2, IHello3 {
 
 /*----------------------------=o=------------------------------*/
 
-abstract class Hello1 {
-    abstract String hello();   
-}
-
-/*----------------------------=o=------------------------------*/
-
 // Classes inheriting multiple unrelated default methods or 
 // a combination of abstract and default methods that are
-// override equivalent must reimplement or redefine the methods
+// override equivalent must reimplement or redeclare the methods
 // in question unless the class extends another class that
 // declares as abstract or implements said methods.
 // 
