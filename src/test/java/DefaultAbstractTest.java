6f716285510dfc040802991bad83e1ebcd91e38d
John Olov Brötell <jolovb@gmail.com>
2015-05-25 15:52:21 -0300

    Added sample showing the 'default-default' and 'default-abstract' inheritance rules.

---
+++ src/test/java/DefaultAbstractTest.java
@@ -0,0 +1,75 @@
+
+interface IHello1 {
+    default String hello() { return "Hello from IHello1."; } 
+}
+
+interface IHello2 {
+    default String hello() { return "Hello from IHello2."; } 
+}
+
+interface IHello3 {
+    abstract String hello();
+}
+
+/*----------------------------=o=------------------------------*/
+
+// Interfaces inheriting multiple unrelated default methods or 
+// a combination of abstract and default methods that are
+// override equivalent must reimplement or redefine the methods
+// in question. The hello() method below may not be omitted.
+
+interface IHello11 extends IHello1, IHello2 {
+    default String hello() { return "Hello from IHello2."; } 
+}
+
+interface IHello12 extends IHello1, IHello2 {
+    abstract String hello();
+}
+
+interface IHello21 extends IHello2, IHello3 {
+    default String hello() { return "Hello from IHello2."; } 
+}
+
+interface IHello22 extends IHello2, IHello3 {
+    abstract String hello();
+}
+
+/*----------------------------=o=------------------------------*/
+
+abstract class Hello1 {
+    abstract String hello();   
+}
+
+/*----------------------------=o=------------------------------*/
+
+// Classes inheriting multiple unrelated default methods or 
+// a combination of abstract and default methods that are
+// override equivalent must reimplement or redefine the methods
+// in question unless the class extends another class that
+// declares as abstract or implements said methods.
+// 
+// The hello() method below may not be omitted unless inherited
+// from a superclass.
+
+abstract class Hello11 implements IHello1, IHello2 {
+    abstract public String hello();
+} 
+
+abstract class Hello12 implements IHello1, IHello2 {
+    public String hello() { return "Hello from Hello12."; }
+}
+
+abstract class Hello13 extends Hello1 
+                       implements IHello1, IHello2 {}
+
+abstract class Hello21 implements IHello2, IHello3 {
+    abstract public String hello();
+}
+
+abstract class Hello22 implements IHello2, IHello3 {
+    public String hello() { return "Hello from Hello22."; }
+}
+
+abstract class Hello23 extends Hello1 
+                       implements IHello2, IHello3 {}
+
